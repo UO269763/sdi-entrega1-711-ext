41c9d5c0891dd8799502da17a1e6316ad1dbe116
UO269763 <UO269763@uniovi.es>
2021-06-15 17:15:37 +0200

    Primer commit

---
+++ sdi-entrega1-711-ext/src/main/java/com/uniovi/SdiEntrega1711ExtApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega1711ExtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiEntrega1711ExtApplication.class, args);
+	}
+
+}
